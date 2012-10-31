142faf42407e2ab2bc3faefc6c727c60c72f35d8
Ben Glasser <soul_trip@hotmail.com>
2012-10-30 19:11:37 -0700

    first commit

---
+++ headerizer/src/com/benglasser/headerizer/Headerizer.java
@@ -0,0 +1,18 @@
+/**
+ * @author Ben Glasser (c) 2012
+ * 
+ */
+
+package com.benglasser.headerizer;
+
+public class Headerizer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
