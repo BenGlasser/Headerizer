692160fba73cb3150648c8cf2c29dd01ac2b0c42
Ben Glasser <soul_trip@hotmail.com>
2012-11-02 01:52:59 -0700

    Begin to add file tree support

--- headerizer/src/com/benglasser/headerizer/Window.java
+++ headerizer/src/com/benglasser/headerizer/Window.java
@@ -23,6 +23,12 @@ import javax.swing.JEditorPane;
 import java.awt.TextArea;
 import javax.swing.JTextArea;
 import java.awt.Window.Type;
+import javax.swing.JMenu;
+import java.awt.TextField;
+import javax.swing.JScrollPane;
+import javax.swing.border.EtchedBorder;
+import java.awt.Label;
+import javax.swing.JCheckBox;
 
 public class Window {
 
@@ -59,47 +65,75 @@ public class Window {
 		frmHeaderizer = new JFrame();
 		frmHeaderizer.setResizable(false);
 		frmHeaderizer.setTitle("Headerizer");
-		frmHeaderizer.setBounds(100, 100, 575, 598);
+		frmHeaderizer.setBounds(100, 100, 819, 688);
 		frmHeaderizer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmHeaderizer.getContentPane().setLayout(null);
 		
 		JButton btnNewButton = new JButton("Headerize!");
-		btnNewButton.setBounds(429, 526, 120, 23);
+		btnNewButton.setBounds(683, 626, 120, 23);
 		frmHeaderizer.getContentPane().add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Help");
-		btnNewButton_1.setBounds(304, 526, 120, 23);
+		btnNewButton_1.setBounds(553, 626, 120, 23);
 		frmHeaderizer.getContentPane().add(btnNewButton_1);
 		
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		tabbedPane.setBounds(10, 11, 539, 504);
+		tabbedPane.setBounds(10, 11, 793, 604);
 		frmHeaderizer.getContentPane().add(tabbedPane);
 		
 		JPanel panel = new JPanel();
-		panel.setBackground(SystemColor.window);
+		panel.setBackground(SystemColor.control);
 		tabbedPane.addTab("General", null, panel, null);
 		panel.setLayout(null);
 		
-		JTree tree = new JTree();
-		tree.setBorder(new LineBorder(new Color(100, 100, 100)));
-		tree.setBounds(264, 75, 260, 390);
-		panel.add(tree);
-		
 		textField = new JTextField();
-		textField.setBounds(10, 44, 415, 20);
+		textField.setBounds(10, 36, 244, 20);
 		panel.add(textField);
 		textField.setColumns(10);
 		
-		JButton btnNewButton_2 = new JButton("Display");
-		btnNewButton_2.setBounds(435, 43, 89, 23);
-		panel.add(btnNewButton_2);
-		
 		JLabel lblDestinationPath = new JLabel("Destination Path or File");
-		lblDestinationPath.setBounds(10, 19, 222, 14);
+		lblDestinationPath.setBounds(10, 11, 222, 14);
 		panel.add(lblDestinationPath);
 		
-		JEditorPane editorPane = new JEditorPane();
-		editorPane.setBounds(10, 75, 244, 158);
-		panel.add(editorPane);
+		JLabel lblEnterTheHeader = new JLabel("Enter the Header you would like to insert here:");
+		lblEnterTheHeader.setBounds(264, 11, 244, 14);
+		panel.add(lblEnterTheHeader);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(SystemColor.control);
+		panel_1.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		panel_1.setBounds(264, 391, 111, 174);
+		panel.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JLabel lblFileTypes = new JLabel("File Types");
+		lblFileTypes.setBounds(28, 12, 48, 14);
+		panel_1.add(lblFileTypes);
+		
+		JCheckBox chckbxcpp = new JCheckBox("cpp");
+		chckbxcpp.setBounds(4, 85, 97, 23);
+		panel_1.add(chckbxcpp);
+		
+		JCheckBox chckbxjava = new JCheckBox("java");
+		chckbxjava.setBounds(4, 111, 97, 23);
+		panel_1.add(chckbxjava);
+		
+		JCheckBox chckbxXml = new JCheckBox("xml");
+		chckbxXml.setBounds(4, 137, 97, 23);
+		panel_1.add(chckbxXml);
+		
+		JCheckBox chckbxC = new JCheckBox("c");
+		chckbxC.setBounds(4, 33, 97, 23);
+		panel_1.add(chckbxC);
+		
+		JCheckBox chckbxCs = new JCheckBox("cs");
+		chckbxCs.setBounds(4, 59, 97, 23);
+		panel_1.add(chckbxCs);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(264, 36, 472, 202);
+		panel.add(textArea);
+
+
 	}
 }
